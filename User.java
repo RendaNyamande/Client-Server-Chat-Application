29972ce435e17ce9e6d84dd6167ed058cb2b37c4
Renda Nyamande <rendanyamande44@gmail.com>
2021-04-12 18:38:34 +0200

    Login done

--- User.java
+++ User.java
@@ -1,16 +1,26 @@
 public class User
 {
     private String userName;
+    private String password;
     private int portNum;
     public User(String name, int num)
     {
          userName = name;
          portNum = num;
     }
+    public User(String name, String password, int num)
+    {
+         userName = name;
+         this.password = password;
+         portNum = num;
+    }
     
     public String getName()
     {
         return userName;
+    }
+    public String getPassword(){
+        return password;
     }
     public int getPort()
     {
